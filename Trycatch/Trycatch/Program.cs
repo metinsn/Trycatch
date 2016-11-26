eefed9a7e72d487c649207bbe84d68c40d3e1ed7
metin.sen <metin.sen@ALS-YG39506-J06>
2016-11-26 17:44:05 +0300

    2

--- Trycatch/Trycatch/Program.cs
+++ Trycatch/Trycatch/Program.cs
@@ -10,39 +10,65 @@ namespace Trycatch
     {
         static void Main(string[] args)
         {
+            #region ornek1
+
             //try
             //{
-
             //    int[] array = new int[4];
             //    Console.WriteLine(array[4]);
-
             //}
             //catch (IndexOutOfRangeException ex)
             //{
-
             //    Console.WriteLine("Hata Oluştu ! " + ex.Message);
             //}
 
+
+            #endregion
+
+            #region ornek2
+            //try
+            //{
+            //    int sayi;
+            //    sayi = Convert.ToInt32(Console.ReadLine());
+            //}
+            ////catch (IndexOutOfRangeException ex)
+            ////{
+            ////    Console.WriteLine("Hata Oluştu ! " + ex.Message);
+            ////}
+            ////catch (FormatException ex)
+            ////{
+            ////    Console.WriteLine("Hata Oluştu ! " + ex.Message);
+            ////}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Hata Oluştu ! " + ex.Message);
+            //}
+            //Console.Read();
+
+            #endregion
+
+            #region ornek2
             try
             {
                 int sayi;
                 sayi = Convert.ToInt32(Console.ReadLine());
             }
-            //catch (IndexOutOfRangeException ex)
-            //{
-            //    Console.WriteLine("Hata Oluştu ! " + ex.Message);
-            //}
-            //catch (FormatException ex)
-            //{
-            //    Console.WriteLine("Hata Oluştu ! " + ex.Message);
-            //}
-
-            catch (Exception ex)
+            catch (OverflowException ex)
             {
                 Console.WriteLine("Hata Oluştu ! " + ex.Message);
-            }            
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Final kısmı çalıştı");
+            }
+                Console.Read();
 
-            Console.Read();
+            #endregion
+
         }
     }
 }
