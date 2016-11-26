d13b65b485c6ca00425f9cd1d76d7688ee0b8dfa
metin.sen <metin.sen@ALS-YG39506-J06>
2016-11-26 17:28:31 +0300

    1

---
+++ Trycatch/Trycatch/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trycatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+
+            //    int[] array = new int[4];
+            //    Console.WriteLine(array[4]);
+
+            //}
+            //catch (IndexOutOfRangeException ex)
+            //{
+
+            //    Console.WriteLine("Hata Oluştu ! " + ex.Message);
+            //}
+
+            try
+            {
+                int sayi;
+                sayi = Convert.ToInt32(Console.ReadLine());
+            }
+            //catch (IndexOutOfRangeException ex)
+            //{
+            //    Console.WriteLine("Hata Oluştu ! " + ex.Message);
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine("Hata Oluştu ! " + ex.Message);
+            //}
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata Oluştu ! " + ex.Message);
+            }            
+
+            Console.Read();
+        }
+    }
+}
